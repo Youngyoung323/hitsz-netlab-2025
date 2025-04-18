f292f39014631afd6f4b18b9cca35d089b4d5ded
Yang Yang <lyf1160025649@gmail.com>
2025-04-18 15:58:51 +0800

    eth finished

--- src/arp.c
+++ src/arp.c
@@ -1,8 +1,11 @@
 #include "arp.h"
 
+#include "buf.h"
 #include "ethernet.h"
+#include "map.h"
 #include "net.h"
 
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 /**
@@ -57,7 +60,25 @@ void arp_print() {
  * @param target_ip 想要知道的目标的ip地址
  */
 void arp_req(uint8_t *target_ip) {
-    // TO-DO
+    // 调用init进行初始化
+    buf_init(&txbuf, sizeof(arp_pkt_t));
+
+    // 填写APR报头
+    arp_pkt_t arp_packet = arp_init_pkt;
+    arp_packet.hw_type16 = swap16(ARP_HW_ETHER);
+    arp_packet.pro_type16 = swap16(NET_PROTOCOL_IP);
+    arp_packet.hw_len = NET_MAC_LEN;
+    arp_packet.pro_len = NET_IP_LEN;
+    arp_packet.opcode16 = swap16(ARP_REQUEST);
+    memcpy(arp_packet.sender_ip, net_if_ip, NET_IP_LEN);
+    memcpy(arp_packet.sender_mac, net_if_mac, NET_MAC_LEN);
+    memcpy(arp_packet.target_ip, target_ip, NET_IP_LEN);
+    
+    // 把报头填充到buf里面
+    memcpy(txbuf.data, &arp_packet, sizeof(arp_pkt_t));
+
+    // 发送APR报文
+    ethernet_out(&txbuf, ether_broadcast_mac, NET_PROTOCOL_ARP);
 }
 
 /**
@@ -67,7 +88,26 @@ void arp_req(uint8_t *target_ip) {
  * @param target_mac 目标mac地址
  */
 void arp_resp(uint8_t *target_ip, uint8_t *target_mac) {
-    // TO-DO
+    // 调用init进行初始化
+    buf_init(&txbuf, sizeof(arp_pkt_t));
+
+    // 填写APR报头
+    arp_pkt_t arp_packet = arp_init_pkt;
+    arp_packet.hw_type16 = swap16(ARP_HW_ETHER);
+    arp_packet.pro_type16 = swap16(NET_PROTOCOL_IP);
+    arp_packet.hw_len = NET_MAC_LEN;
+    arp_packet.pro_len = NET_IP_LEN;
+    arp_packet.opcode16 = swap16(ARP_REPLY);
+    memcpy(arp_packet.sender_ip, net_if_ip, NET_IP_LEN);
+    memcpy(arp_packet.sender_mac, net_if_mac, NET_MAC_LEN);
+    memcpy(arp_packet.target_ip, target_ip, NET_IP_LEN);
+    memcpy(arp_packet.target_mac, target_mac, NET_MAC_LEN);
+     
+    // 把报头填充到buf里面
+    memcpy(txbuf.data, &arp_packet, sizeof(arp_pkt_t));
+ 
+    // 发送APR报文
+    ethernet_out(&txbuf, target_mac, NET_PROTOCOL_ARP);
 }
 
 /**
@@ -77,7 +117,36 @@ void arp_resp(uint8_t *target_ip, uint8_t *target_mac) {
  * @param src_mac 源mac地址
  */
 void arp_in(buf_t *buf, uint8_t *src_mac) {
-    // TO-DO
+    // 检查数据报长度是否小于APR的头部长度
+    if(buf->len < sizeof(arp_pkt_t)) return;
+
+    // 检查报头数据
+    arp_pkt_t *arp_pkt = (arp_pkt_t*) buf->data;
+    
+    if((arp_pkt->hw_type16 != swap16(ARP_HW_ETHER)) || 
+       (arp_pkt->pro_type16 != swap16(NET_PROTOCOL_IP)) ||
+       (arp_pkt->hw_len != NET_MAC_LEN) ||
+       (arp_pkt->pro_len != NET_IP_LEN) ||
+       (arp_pkt->opcode16 != swap16(ARP_REQUEST) && arp_pkt->opcode16 != swap16(ARP_REPLY))) return;
+
+    // 更新ARP表项
+    map_set(&arp_table, arp_pkt->sender_ip, src_mac);
+
+    // 调用map_get获取arp_buf中的数据包
+    buf_t *_arp_buf = (buf_t*)map_get(&arp_buf, arp_pkt->sender_ip);
+
+    // 查看arp_buf中是否有对应的缓存
+    if(_arp_buf != NULL) {
+        // 有缓存就先处理该数据包,这个数据包是我们上次没找到MAC导致缓存下来的
+        ethernet_out(_arp_buf, arp_pkt->sender_mac, NET_PROTOCOL_IP);
+        map_delete(&arp_buf, arp_pkt->sender_ip);
+        return;
+    }
+    else {
+        if(arp_pkt->opcode16 == swap16(ARP_REQUEST) && !memcpy(arp_pkt->target_ip, net_if_ip, NET_IP_LEN)) {
+            arp_resp(arp_pkt->sender_ip, arp_pkt->sender_mac);
+        }
+    }
 }
 
 /**
@@ -88,7 +157,19 @@ void arp_in(buf_t *buf, uint8_t *src_mac) {
  * @param protocol 上层协议
  */
 void arp_out(buf_t *buf, uint8_t *ip) {
-    // TO-DO
+    // 查找ARP表,根据IP地址查找MAC地址
+    uint8_t *mac_address = (uint8_t *)map_get(&arp_table, ip);
+
+    // 如果能找到MAC就直接发出去
+    if(mac_address != NULL) {
+        ethernet_out(buf, mac_address, NET_PROTOCOL_IP);  // 这是发给以太网层的
+    }
+    else { // 没找到则需要进一步处理,需要判断buf此时是否有包(有就说明已经有arp请求了)
+        if(map_get(&arp_buf, ip) == NULL) {
+            map_set(&arp_buf, ip, buf);  // map原始的键值对中值存的是数据包
+            arp_req(ip);
+        }
+    }
 }
 
 /**
--- src/ethernet.c
+++ src/ethernet.c
@@ -1,16 +1,30 @@
 #include "ethernet.h"
 
 #include "arp.h"
+#include "buf.h"
 #include "driver.h"
 #include "ip.h"
+#include "net.h"
 #include "utils.h"
+#include <stdint.h>
 /**
  * @brief 处理一个收到的数据包
  *
  * @param buf 要处理的数据包
  */
 void ethernet_in(buf_t *buf) {
-    // TO-DO
+    if(buf->len < sizeof(ether_hdr_t)) return;  // 数据包不完整,直接丢弃
+
+    // 先获取src和protocol
+    uint16_t *protocolptr = (uint16_t*) (buf->data + 12);
+    uint16_t  protocol    = swap16(*protocolptr);  // 要从大端变成小端
+    uint8_t  *mac         = (uint8_t*) (buf->data + 6);
+
+    // 调用buf_remove_header移除包头
+    buf_remove_header(buf, sizeof(ether_hdr_t));
+
+    // 调用net_in向上层传递数据包
+    net_in(buf, protocol, mac);
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -20,7 +34,21 @@ void ethernet_in(buf_t *buf) {
  * @param protocol 上层协议
  */
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol) {
-    // TO-DO
+    // 判断是否需要进行填充
+    if(buf->len < ETHERNET_MIN_TRANSPORT_UNIT) {
+        buf_add_padding(buf, ETHERNET_MIN_TRANSPORT_UNIT - buf->len);
+    }
+
+    // 添加以太网包头
+    buf_add_header(buf, sizeof(ether_hdr_t));
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+
+    // 目的MAC地址、源MAC以及协议类型protocol
+    memcpy(hdr->dst, mac, NET_MAC_LEN);
+    memcpy(hdr->src, net_if_mac, NET_MAC_LEN);
+    hdr->protocol16 = swap16(protocol);  // 发送出去要从小端转换成大端
+
+    driver_send(buf);  // send eth
 }
 /**
  * @brief 初始化以太网协议
